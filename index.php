e23f56e7619e24cb7e9e4524e7968c10906f871f
jlsamantila <boybudz326@gmail.com>
2025-03-06 12:06:15 +0800

    Add main index file that includes all components

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'includes/header.php';
+
+// Include all components
+include_once 'components/hero.php';
+include_once 'components/about.php';
+include_once 'components/skills.php';
+include_once 'components/experience.php';
+include_once 'components/projects.php';
+include_once 'components/consulting.php';
+include_once 'components/contact.php';
+
+require_once 'includes/footer.php';
+?>
